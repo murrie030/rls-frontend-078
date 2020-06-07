98be3dad63d5fc79ba03829578bfd1a534b40309
murrie030 <mustafa.toprak@student.hu.nl>
2020-06-07 19:53:44 +0200

    Home component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+import Home from "./components/home";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
+);

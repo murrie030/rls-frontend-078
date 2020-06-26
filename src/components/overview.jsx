896a26900a8e191e829b4b27ef96c780b8bf38e0
murrie030 <mustafa.toprak@student.hu.nl>
2020-06-26 21:23:12 +0200

    Overview vehicles done

--- src/components/overview.jsx
+++ src/components/overview.jsx
@@ -4,39 +4,55 @@ import "./buttons/createButton";
 import CreateButton from "./buttons/createButton";
 
 class Overview extends Component {
-  state = {};
   constructor(props) {
     super(props);
 
     this.state = {
-      data: null,
+      data: [],
     };
   }
+
+  /* 
+    When the data is fetched successfully, it will be storde
+    in the local state with React's this.setState() method 
+  */
+  componentDidMount() {
+    fetch("http://localhost:5000/api/v1/vehicles")
+      .then((response) => response.json())
+      .then((data) => this.setState({ data: data.data }));
+  }
+
   render() {
     return (
       <div className="container" id="content">
         <h1>Overview</h1>
+        <p>
+          Below there is an overview of the <strong>vehicles</strong>.
+        </p>
         <CreateButton />
+
         <table className="table">
           <thead>
             <tr>
               <th className="title" scope="col">
-                Operational Train Number
+                Vehicle ID
               </th>
               <th className="title" scope="col">
-                Train Type
+                Internal Vehicle Facility
               </th>
               <th className="title" scope="col">
-                Company
+                Information
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Passenger Train</td>
-              <td>NS</td>
-            </tr>
+            {this.state.data.map((vehicle) => (
+              <tr key={vehicle.id}>
+                <th scope="row">{vehicle.id}</th>
+                <td>{vehicle.internalVehicleFacility}</td>
+                <td>{vehicle.informationToBeDisplayed}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
